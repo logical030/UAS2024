44ab5c88b9274e0c30c1e3800496afa9faa22f49
logical030 <115705693+logical030@users.noreply.github.com>
2024-02-17 15:56:04 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+data = pd.read_csv('data-kejadian-bencana-banjir.csv')
+
+# Pertanyaan 1: Total Nilai Kerugian per Wilayah
+st.subheader('Pertanyaan 1: Total Nilai Kerugian per Wilayah')
+
+nilai_kerugian = data.groupby('wilayah')['nilai_kerugian'].sum().reset_index()
+
+plt.figure()
+plt.bar(nilai_kerugian['wilayah'], nilai_kerugian['nilai_kerugian'])
+plt.title('Total Nilai Kerugian per Wilayah')
+plt.xlabel('Wilayah')
+plt.ylabel('Total Nilai Kerugian (Rp)')
+st.pyplot(plt)
+
+# Pertanyaan 2: Jumlah Pengungsi per Bulan
+st.subheader('Pertanyaan 2: Jumlah Pengungsi per Bulan')
+
+data['bulan'] = pd.to_datetime(data['bulan'], format='%m').dt.month_name()
+
+pengungsi = data.groupby('bulan')['jumlah_pengungsi'].sum().reset_index()
+
+plt.figure()
+plt.plot(pengungsi['bulan'], pengungsi['jumlah_pengungsi'])
+plt.title('Jumlah Pengungsi per Bulan')
+plt.xlabel('Bulan')
+plt.ylabel('Jumlah Pengungsi')
+st.pyplot(plt)
